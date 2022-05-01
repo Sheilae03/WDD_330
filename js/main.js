825d7b3360cd34c2f2e588b7ddff3e600b595d98
Sheilae03 <72531575+Sheilae03@users.noreply.github.com>
2022-04-30 21:54:31 -0500

    Updated main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
 const links = [
     {
       label: "Week1 notes",
-      url: "week1/index.html"
+      url: "week1/index.html",
+    
+      label:"Week2 notes",
+      url: "week2/index.html"
     }
+    
   ]
   function loadIndex() {
      const ol = document.querySelector("#linksList");
