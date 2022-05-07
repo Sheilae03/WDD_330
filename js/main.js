7b988195eb8c77a1b6274c8b39e5948556622904
Sheilae03 <72531575+Sheilae03@users.noreply.github.com>
2022-05-07 15:32:23 -0500

    Week 3 Notes

--- js/main.js
+++ js/main.js
@@ -2,10 +2,16 @@ const links = [
     {
       label: "Week1 notes",
       url: "week1/index.html",
-    
+
       label:"Week2 notes",
+      url: "week2/index.html",
+
+      label:"Week3 notes",
       url: "week2/index.html"
+
+
     }
+    
     
   ]
   function loadIndex() {
