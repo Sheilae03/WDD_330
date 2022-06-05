c6241e155a3836de9a44b0ea0c7f4ee6d24d1c81
Sheilae03 <72531575+Sheilae03@users.noreply.github.com>
2022-06-05 00:25:54 -0500

    fixed main.js

--- js/main.js
+++ js/main.js
@@ -1,21 +1,27 @@
 const links = [
     {
       label: "Week1 notes",
-      url: "week1/index.html",
+      url: "week1/index.html"
+    },
+      
 
-      label:"Week2 notes",
-      url: "week2/index.html",
+     {label:"Week2 notes",
+      url: "week2/index.html"
+    },
 
-      label:"Week3 notes",
-      url: "week3/index.html",
+     { label:"Week3 notes",
+      url: "week3/index.html"
+    },
 
-      label:"Week4 notes",
-      url: "week4/index.html",
+     { label:"Week4 notes",
+      url: "week4/index.html"
+    },
 
-      label:"Week5 notes",
-      url: "week5/index.html",
+      {label:"Week5 notes",
+      url: "week5/index.html"}
+      ,
 
-      label:"Week7 notes",
+      {label:"Week7 notes",
       url: "week7/index.html"
     }
     
