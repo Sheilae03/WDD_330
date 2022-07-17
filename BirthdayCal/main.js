365e964f8984cbd0cc0b26c7bbe2259751547630
Sheilae03 <72531575+Sheilae03@users.noreply.github.com>
2022-07-16 23:50:30 -0500

    updated work

--- BirthdayCal/main.js
+++ BirthdayCal/main.js
@@ -1,20 +1,45 @@
 window.addEventListener('load', () => {
-    dates = JSON.parse(localStorage.getItem('todos')) || [];
-    const newDateForm = document.querySelector('new-date-form')
+    dates = JSON.parse(localStorage.getItem('dates')) || [];
+    const newDateForm = document.querySelector('#new-date-form');
 
-    newDateForm.addEventListener('submit', e => {
-        e.preventDefault();
-        const date = {
-        content: e.target.elements.content.value,
-        done: false}
+newDateForm.addEventListener('submit', e => {
+    e.preventDefault();
 
-        dates.push(todo);
+    const date = {
+        event: e.target.elements.event.value,
+        month: e.target.elements.month.value,
+        day: e.target.elements.day.value,
+        fname: e.target.elements.fname.value,
+        lname: e.target.elements.lname.value,
+    
+        createdAt:new Date().getTime()
+    }
 
-        localStorage.setItem('todos', JSON.stringify(dates));
+
+    dates.push(date);
+
+        localStorage.setItem('dates', JSON.stringify(dates));
         e.target.reset();
         
-        DisplayDates();
+        // DisplayDates();
 
     })
-
 })
+    
+   /*  function DisplayDates() {
+        const datesList = document.querySelector('date_list');
+    
+        datesList.innerHTML = '';
+
+        dates.forEach(dates => {
+            const dateItem = document.createElement('div');
+            dateItem.classList.add('todo_item')
+
+            const event = document.createElement('event');
+            const month = document.createElement('month');
+            const date = document.createElement('date');
+            const fname = document.createElement('fname');
+            const lname = document.createElement('lname');
+           
+        })} */
+
