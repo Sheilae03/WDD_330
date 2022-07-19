384f6827fe4ee338d7af0ae5a5078bc9de1ef325
Sheilae03 <72531575+Sheilae03@users.noreply.github.com>
2022-07-19 16:10:43 -0500

    another update

--- BirthdayCal/main.js
+++ BirthdayCal/main.js
@@ -21,25 +21,25 @@ newDateForm.addEventListener('submit', e => {
         localStorage.setItem('dates', JSON.stringify(dates));
         e.target.reset();
         
-        // DisplayDates();
+       // DisplayDates();
 
     })
 })
     
-   /*  function DisplayDates() {
+     function DisplayDates() {
         const datesList = document.querySelector('date_list');
     
         datesList.innerHTML = '';
 
         dates.forEach(dates => {
             const dateItem = document.createElement('div');
-            dateItem.classList.add('todo_item')
+            dateItem.classList.add('date_list')
 
             const event = document.createElement('event');
             const month = document.createElement('month');
-            const date = document.createElement('date');
+            const day = document.createElement('day');
             const fname = document.createElement('fname');
             const lname = document.createElement('lname');
            
-        })} */
+        })} 
 
